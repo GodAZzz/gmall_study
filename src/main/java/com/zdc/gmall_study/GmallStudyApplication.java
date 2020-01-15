b3334e416f8845bbaaa19e3f1322ac804cdc57ec
1 <1>
2020-01-15 09:11:06 +0800

    Initial commit

---
+++ src/main/java/com/zdc/gmall_study/GmallStudyApplication.java
@@ -0,0 +1,13 @@
+package com.zdc.gmall_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmallStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zdc/gmall_study/GmallStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zdc.gmall_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
